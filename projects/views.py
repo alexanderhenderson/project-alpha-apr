b3751fef2fd0b20793e507e7a665f19c7f1dff51
Alexander Henderson <alex.henderson92@yahoo.com>
2022-06-17 18:13:39 -0700

    Feature 8 complete

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 
+from django.urls.base import reverse_lazy
+
 from django.views.generic.list import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from projects.models import Project
 
 # Create your views here.
 
 
-class ProjectListView(ListView):
+class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "projects/list.html"
     context_object_name = "project_list"
+    login_url = reverse_lazy('login')
+
+    def get_queryset(self):
+        user = self.request.user
+        return Project.objects.filter(members=user)
+    
