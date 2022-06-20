70419075c4590b007fa9595e80beeb4bec7f89d6
Alexander Henderson <alex.henderson92@yahoo.com>
2022-06-20 14:33:27 -0700

    Feature 11 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from django.contrib.auth.views import LoginView, LogoutView
 
+from accounts.views import SigninView
+
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('signup/', SigninView.as_view(), name='signup'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.views.generic import FormView
+
+from django.urls.base import reverse_lazy
+from django.http.response import HttpResponseRedirect
+
 # Create your views here.
+
+
+class SigninView(FormView):
+
+    form_class = UserCreationForm
+    template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+
+        form.save()
+        user = form.save()
+        login(
+            self.request,
+            user,
+            backend='django.contrib.auth.backends.ModelBackend'
+            )
+
+        return HttpResponseRedirect(reverse_lazy('home'))
