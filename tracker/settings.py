ce25a63a186995b3953d180649fc1d1630b091f7
Alexander Henderson <alex.henderson92@yahoo.com>
2022-06-20 20:03:21 -0700

    Feature 18 complete

--- tracker/settings.py
+++ tracker/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'markdownify.apps.MarkdownifyConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Markdownify Settings
+
+MARKDOWNIFY = {
+    "default": {
+        "BLEACH": False
+    }
+}
