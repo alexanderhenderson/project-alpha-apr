9d7c92a4e9008562ac4fb76cb4428a74a6f29c5e
Alexander Henderson <alex.henderson92@yahoo.com>
2022-06-20 18:04:31 -0700

    Feature 14 complete

--- projects/urls.py
+++ projects/urls.py
@@ -4,10 +4,12 @@ from projects.views import (
 
     ProjectListView,
     ProjectDetailView,
+    ProjectCreateView,
 
     )
 
 urlpatterns = [
     path("", ProjectListView.as_view(), name="list_projects"),
-    path('<int:pk>/', ProjectDetailView.as_view(), name='show_project')
+    path('<int:pk>/', ProjectDetailView.as_view(), name='show_project'),
+    path('create/', ProjectCreateView.as_view(), name='create_project'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 
 from django.urls.base import reverse_lazy
 
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from pkg_resources import compatible_platforms
 
 from projects.models import Project
 
@@ -20,10 +22,20 @@ class ProjectListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         user = self.request.user
         return Project.objects.filter(members=user)
-    
+
 
 class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
     template_name = 'projects/detail.html'
     context_object_name = "project_list"
     login_url = reverse_lazy('login')
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = 'projects/create.html'
+    context_object_name = "project_list"
+    fields = ['name', 'description', 'members']
+
+    def get_success_url(self):
+        return reverse_lazy("show_project", kwargs={'pk': self.object.pk})
